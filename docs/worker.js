4625b6c7c81a9a583729c3b85d48dc8724cc4a6e
Jack Kirby <jackrekirby@gmail.com>
2022-06-05 12:00:41 +0100

    readded raytracer worker

--- docs/worker.js
+++ docs/worker.js
@@ -1,27 +1,23 @@
 import init, { run_raytracer } from "./raytracer_lib.js";
 
 onmessage = function (e) {
-    console.log('worker');
+    const fnc = {
+        init: () => {
+            init().then(() => postMessage({ action: 'init', complete: true }));
+        },
+        render: () => {
+            const a = e.data.arguments;
+            console.log(a);
+            const output = run_raytracer(
+                a.width,
+                a.height,
+                a.samples,
+                a.depth,
+                a.objects
+            );
+            postMessage({ action: 'render', start: e.data.start, output: output });
+        }
+
+    }
+    fnc[e.data.action]();
 }
-
-// onmessage = function (e) {
-//     const fnc = {
-//         init: () => {
-//             init().then(() => postMessage({ action: 'init', complete: true }));
-//         },
-//         render: () => {
-//             const a = e.data.arguments;
-//             console.log(a);
-//             const output = run_raytracer(
-//                 a.width,
-//                 a.height,
-//                 a.samples,
-//                 a.depth,
-//                 a.objects
-//             );
-//             postMessage({ action: 'render', start: e.data.start, output: output });
-//         }
-
-//     }
-//     fnc[e.data.action]();
-// }
